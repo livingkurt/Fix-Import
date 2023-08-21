b5dc14ec721aa7f37bd7b7d4c06c99447d8cda40
Kurt LaVacque <lavacquek@icloud.com>
2023-08-21 17:32:14 -0500

    Trying to get the new version of Material UI V5 imports

--- src/extension.ts
+++ src/extension.ts
@@ -42,3 +42,33 @@ function exactImports(text: string): string {
   // Replace the matched import statements with the transformed ones
   return text.replace(importRegex, replacer);
 }
+
+// function exactImports(text: string): string {
+//   // Regular expression to match Material-UI v4, v5, and lodash import statements
+//   const importRegex = /import\s+{([^}]+)}\s+from\s+'((@material-ui\/core|@material-ui\/lab|@mui\/material|@mui\/lab|lodash)[^']*)';/g;
+
+//   // Function to transform each matched import statement
+//   const replacer = (match: string, p1: string, p2: string) => {
+//     const imports = p1.split(",").map(s => s.trim());
+
+//     // Handle MUI v5 imports
+//     if (p2.startsWith("@mui/material") || p2.startsWith("@mui/lab")) {
+//       return imports
+//         .map(imp => `import ${imp} from '${p2}/${imp}';`)
+//         .join("\n");
+//     }
+
+//     // Handle MUI v4 and lodash imports
+//     if (p2.startsWith("@material-ui") || p2.startsWith("lodash")) {
+//       return imports
+//         .map(imp => `import ${imp} from '${p2}/${imp}';`)
+//         .join("\n");
+//     }
+
+//     // If the import is not from Material-UI or lodash, return the original match
+//     return match;
+//   };
+
+//   // Replace the matched import statements with the transformed ones
+//   return text.replace(importRegex, replacer);
+// }
